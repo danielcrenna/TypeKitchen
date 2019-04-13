a77cf4c7a8cc7ac79bb889a1bd68413144e28389
Daniel Crenna <daniel.crenna@gmail.com>
2019-04-12 23:33:03 -0400

    remove unused class for now

--- src/TypeKitchen/Internal/Pools.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Blowdart, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-namespace TypeKitchen.Internal
-{
-    internal static class Pools
-    {
-    }
-}
