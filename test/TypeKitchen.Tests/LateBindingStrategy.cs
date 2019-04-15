92a940f9340627e8ff024e5aa0889603aa324e62
Daniel Crenna <daniel.crenna@gmail.com>
2019-04-15 00:12:53 -0400

    LateBindingStrategy is a test-only enum

--- test/TypeKitchen.Tests/LateBindingStrategy.cs
+++ test/TypeKitchen.Tests/LateBindingStrategy.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Blowdart, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace TypeKitchen
+namespace TypeKitchen.Tests
 {
     public enum LateBindingStrategy
     {
