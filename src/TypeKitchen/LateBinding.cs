f7b70afa824706a70acc70fe79b56945b0dc12ce
Daniel Crenna <daniel.crenna@gmail.com>
2019-04-15 17:29:51 -0400

    merge LateBinding code

--- src/TypeKitchen/LateBinding.cs
+++ src/TypeKitchen/LateBinding.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿// Copyright (c) Blowdart, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
@@ -11,116 +15,55 @@ namespace TypeKitchen
 {
     public static partial class LateBinding
     {
-        #region Method Invoke
+        #region CallSite 
 
-        public static Func<object, object> MethodInvokeBindGet(AccessorMember member)
+        public static Dictionary<string, Func<object, object>> CallSiteBindGet(AccessorMembers members)
         {
-            switch (member.MemberInfo)
-            {
-                case PropertyInfo property:
-                {
-                    var getMethod = property.GetGetMethod();
-                    if (getMethod == null)
-                        throw new ArgumentNullException();
-                    return o => getMethod.Invoke(o, null);
-                }
-                case FieldInfo field:
-                {
-                    return o => field.GetValue(o);
-                }
-                default:
-					throw new ArgumentException();
-            }
+            return members.Members.Where(member => member.CanRead)
+                .ToDictionary(member => member.Name, CallSiteBindGet);
         }
 
-        public static Action<object, object> MethodInvokeBindSet(AccessorMember member)
+        public static Dictionary<string, Action<object, object>> CallSiteBindSet(AccessorMembers members)
         {
-            switch (member.MemberInfo)
-            {
-                case PropertyInfo property:
-                {
-                    var setMethod = property.GetSetMethod();
-                    if (setMethod == null)
-                        throw new ArgumentNullException();
-                    return (o, v) => setMethod.Invoke(o, new[] { v });
-                }
-                case FieldInfo field:
-                    return (o, v) => field.SetValue(o, v);
-                default:
-                    throw new ArgumentException();
-            }
+            return members.Members.Where(member => member.CanWrite)
+                .ToDictionary(member => member.Name, CallSiteBindSet);
         }
 
-        #endregion
-
-        #region Expression
-
-        public static Func<object, object> ExpressionBindGet(AccessorMember member)
+        public static Func<object, object> CallSiteBindGet(AccessorMember member)
         {
-            var declaringType = member.MemberInfo.DeclaringType;
-            if (declaringType == null)
-                throw new ArgumentNullException();
-
-            switch (member.MemberInfo)
-            {
-                case PropertyInfo property:
-                {
-                    var getMethod = property.GetGetMethod();
-                    if (getMethod == null)
-                        throw new ArgumentNullException();
-                    var targetParam = Expression.Parameter(typeof(object), "target");
-                    var call = Expression.Call(Expression.Convert(targetParam, declaringType), getMethod);
-                    var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(call, typeof(object)), targetParam);
-                    return lambda.Compile();
-                }
-                case FieldInfo field:
-                {
-                    var targetParam = Expression.Parameter(typeof(object), "target");
-                    var getField = Expression.Field(Expression.Convert(targetParam, declaringType), field);
-                    var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(getField, typeof(object)), targetParam);
-                    return lambda.Compile();
-                }
-                default:
-                    throw new ArgumentException();
-            }
+            var args = new List<CSharpArgumentInfo> { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) };
+            var binder = Microsoft.CSharp.RuntimeBinder.Binder.GetMember(CSharpBinderFlags.None, member.Name, member.MemberInfo.DeclaringType, args);
+            var callsite = CallSite<Func<CallSite, object, object>>.Create(binder);
+            return t => callsite.Target(callsite, t);
         }
 
-        public static Action<object, object> ExpressionBindSet(AccessorMember member)
+        public static Action<object, object> CallSiteBindSet(AccessorMember member)
         {
-            var declaringType = member.MemberInfo.DeclaringType;
-            if (declaringType == null)
-                throw new ArgumentNullException();
-
-            switch (member.MemberInfo)
+            var args = new List<CSharpArgumentInfo>
             {
-                case PropertyInfo property:
-                    {
-                        var setMethod = property.GetSetMethod();
-                        if (setMethod == null)
-                            throw new ArgumentNullException();
-                        
-                        var targetParam = Expression.Parameter(typeof(object), "target");
-                        var valueParam = Expression.Parameter(typeof(object), "value");
-                        var call = Expression.Call(Expression.Convert(targetParam, declaringType), setMethod, Expression.Convert(valueParam, property.PropertyType));
-                        var lambda = Expression.Lambda<Action<object, object>>(call, targetParam, valueParam);
-                        return lambda.Compile();
-                    }
-                case FieldInfo field:
-                    {
-                        var targetParam = Expression.Parameter(typeof(object), "target");
-                        var valueParam = Expression.Parameter(typeof(object), "value");
-                        var setField = Expression.Field(Expression.Convert(targetParam, declaringType), field);
-                        var lambda = Expression.Lambda<Action<object, object>>(Expression.Convert(Expression.Assign(setField, Expression.Convert(valueParam, field.FieldType)), typeof(object)), targetParam, valueParam);
-                        return lambda.Compile();
-                    }
-                default:
-                    throw new ArgumentException();
-            }
+                CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null),
+                CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)
+            };
+            var binder = Microsoft.CSharp.RuntimeBinder.Binder.SetMember(CSharpBinderFlags.None, member.Name, member.MemberInfo.DeclaringType, args);
+            var callsite = CallSite<Action<CallSite, object, object>>.Create(binder);
+            return (t, v) => callsite.Target(callsite, t, v);
         }
 
         #endregion
 
         #region DynamicMethod
+
+        public static Dictionary<string, Func<object, object>> DynamicMethodBindGet(AccessorMembers members)
+        {
+            return members.Members.Where(member => member.CanRead)
+                .ToDictionary(member => member.Name, DynamicMethodBindGet);
+        }
+
+        public static Dictionary<string, Action<object, object>> DynamicMethodBindSet(AccessorMembers members)
+        {
+            return members.Members.Where(member => member.CanWrite)
+                .ToDictionary(member => member.Name, DynamicMethodBindSet);
+        }
 
         public static Func<object, object> DynamicMethodBindGet(AccessorMember member)
         {
@@ -130,16 +73,16 @@ namespace TypeKitchen
             switch (member.MemberInfo)
             {
                 case PropertyInfo property:
-                {
-                    var getMethod = property.GetGetMethod();
-                    if (getMethod == null)
-                        throw new ArgumentNullException();
-                    il.Emit(OpCodes.Ldarg_0);
-                    il.Emit(getMethod.IsFinal || !getMethod.IsVirtual ? OpCodes.Call : OpCodes.Callvirt, getMethod);
-                    if (property.PropertyType.IsValueType)
-                        il.Emit(OpCodes.Box, property.PropertyType);
-                    break;
-                }
+                    {
+                        var getMethod = property.GetGetMethod();
+                        if (getMethod == null)
+                            throw new ArgumentNullException();
+                        il.Emit(OpCodes.Ldarg_0);
+                        il.Emit(getMethod.IsFinal || !getMethod.IsVirtual ? OpCodes.Call : OpCodes.Callvirt, getMethod);
+                        if (property.PropertyType.IsValueType)
+                            il.Emit(OpCodes.Box, property.PropertyType);
+                        break;
+                    }
                 case FieldInfo field:
                     if (field.IsStatic)
                     {
@@ -205,34 +148,23 @@ namespace TypeKitchen
 
         #endregion
 
-        #region CallSite 
-
-        public static Func<object, object> CallSiteBindGet(AccessorMember member)
-        {
-            var args = new List<CSharpArgumentInfo> { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) };
-            var binder = Binder.GetMember(CSharpBinderFlags.None, member.Name, member.MemberInfo.DeclaringType, args);
-            var callsite = CallSite<Func<CallSite, object, object>>.Create(binder);
-            return t => callsite.Target(callsite, t);
-        }
-
-        public static Action<object, object> CallSiteBindSet(AccessorMember member)
-        {
-            var args = new List<CSharpArgumentInfo>
-            {
-                CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null),
-                CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)
-            };
-            var binder = Binder.SetMember(CSharpBinderFlags.None, member.Name, member.MemberInfo.DeclaringType, args);
-            var callsite = CallSite<Action<CallSite, object, object>>.Create(binder);
-            return (t, v) => callsite.Target(callsite, t, v);
-        }
-
-        #endregion
-
         #region Open Delegates
 
-        public static Func<object, object> OpenDelegateBindGet<TTarget>(AccessorMember member)
+        public static Dictionary<string, Func<object, object>> OpenDelegateBindGet<TTarget>(AccessorMembers members)
             where TTarget : class
+        {
+            return members.Members.Where(member => member.CanRead)
+                .ToDictionary(member => member.Name, OpenDelegateBindGet<TTarget>);
+        }
+
+        public static Dictionary<string, Action<object, object>> OpenDelegateBindSet<TTarget>(AccessorMembers members)
+            where TTarget : class
+        {
+            return members.Members.Where(member => member.CanWrite)
+                .ToDictionary(member => member.Name, OpenDelegateBindSet<TTarget>);
+        }
+
+        public static Func<object, object> OpenDelegateBindGet<TTarget>(AccessorMember member) where TTarget : class
         {
             if (member.Type.IsValueType)
                 throw new NotImplementedException();
@@ -303,7 +235,7 @@ namespace TypeKitchen
                                 {
                                     var action = Delegate.CreateDelegate(
                                         typeof(Action<,>).MakeGenericType(type, parameters[0].ParameterType), setProperty);
-                                    setter = (Action<TTarget, object>) CallSetWithValue
+                                    setter = (Action<TTarget, object>)CallSetWithValue
                                         .MakeGenericMethod(type, parameters[0].ParameterType)
                                         .Invoke(null, new object[] { action });
                                     break;
@@ -312,7 +244,7 @@ namespace TypeKitchen
                                 {
                                     var action = Delegate.CreateDelegate(
                                         typeof(Action<,>).MakeGenericType(type, parameters[0].ParameterType), setProperty);
-                                    setter = (Action<TTarget, object>) CallSetWithValue
+                                    setter = (Action<TTarget, object>)CallSetWithValue
                                         .MakeGenericMethod(type, parameters[0].ParameterType)
                                         .Invoke(null, new object[] { action });
                                     break;
@@ -373,6 +305,139 @@ namespace TypeKitchen
         }
 
         #endregion
+
+        #endregion
+
+        #region Expression
+
+        public static Dictionary<string, Func<object, object>> ExpressionBindGet(AccessorMembers members)
+        {
+            return members.Members.Where(member => member.CanRead)
+                .ToDictionary(member => member.Name, ExpressionBindGet);
+        }
+
+        public static Dictionary<string, Action<object, object>> ExpressionBindSet(AccessorMembers members)
+        {
+            return members.Members.Where(member => member.CanWrite)
+                .ToDictionary(member => member.Name, ExpressionBindSet);
+        }
+
+        public static Func<object, object> ExpressionBindGet(AccessorMember member)
+        {
+            var declaringType = member.MemberInfo.DeclaringType;
+            if (declaringType == null)
+                throw new ArgumentNullException();
+
+            switch (member.MemberInfo)
+            {
+                case PropertyInfo property:
+                    {
+                        var getMethod = property.GetGetMethod();
+                        if (getMethod == null)
+                            throw new ArgumentNullException();
+                        var targetParam = Expression.Parameter(typeof(object), "target");
+                        var call = Expression.Call(Expression.Convert(targetParam, declaringType), getMethod);
+                        var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(call, typeof(object)), targetParam);
+                        return lambda.Compile();
+                    }
+                case FieldInfo field:
+                    {
+                        var targetParam = Expression.Parameter(typeof(object), "target");
+                        var getField = Expression.Field(Expression.Convert(targetParam, declaringType), field);
+                        var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(getField, typeof(object)), targetParam);
+                        return lambda.Compile();
+                    }
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static Action<object, object> ExpressionBindSet(AccessorMember member)
+        {
+            var declaringType = member.MemberInfo.DeclaringType;
+            if (declaringType == null)
+                throw new ArgumentNullException();
+
+            switch (member.MemberInfo)
+            {
+                case PropertyInfo property:
+                    {
+                        var setMethod = property.GetSetMethod();
+                        if (setMethod == null)
+                            throw new ArgumentNullException();
+
+                        var targetParam = Expression.Parameter(typeof(object), "target");
+                        var valueParam = Expression.Parameter(typeof(object), "value");
+                        var call = Expression.Call(Expression.Convert(targetParam, declaringType), setMethod, Expression.Convert(valueParam, property.PropertyType));
+                        var lambda = Expression.Lambda<Action<object, object>>(call, targetParam, valueParam);
+                        return lambda.Compile();
+                    }
+                case FieldInfo field:
+                    {
+                        var targetParam = Expression.Parameter(typeof(object), "target");
+                        var valueParam = Expression.Parameter(typeof(object), "value");
+                        var setField = Expression.Field(Expression.Convert(targetParam, declaringType), field);
+                        var lambda = Expression.Lambda<Action<object, object>>(Expression.Convert(Expression.Assign(setField, Expression.Convert(valueParam, field.FieldType)), typeof(object)), targetParam, valueParam);
+                        return lambda.Compile();
+                    }
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        #endregion
+
+        #region Method Invoke
+
+        public static Dictionary<string, Func<object, object>> MethodInvokeBindGet(AccessorMembers members)
+        {
+            return members.Members.Where(member => member.CanRead)
+                .ToDictionary(member => member.Name, MethodInvokeBindGet);
+        }
+
+        public static Dictionary<string, Action<object, object>> MethodInvokeBindSet(AccessorMembers members)
+        {
+            return members.Members.Where(member => member.CanRead)
+                .ToDictionary(member => member.Name, MethodInvokeBindSet);
+        }
+
+        public static Func<object, object> MethodInvokeBindGet(AccessorMember member)
+        {
+            switch (member.MemberInfo)
+            {
+                case PropertyInfo property:
+                {
+                    var getMethod = property.GetGetMethod();
+                    if (getMethod == null)
+                        throw new ArgumentNullException();
+                    return o => getMethod.Invoke(o, null);
+                }
+                case FieldInfo field:
+                {
+                    return o => field.GetValue(o);
+                }
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static Action<object, object> MethodInvokeBindSet(AccessorMember member)
+        {
+            switch (member.MemberInfo)
+            {
+                case PropertyInfo property:
+                {
+                    var setMethod = property.GetSetMethod();
+                    if (setMethod == null)
+                        throw new ArgumentNullException();
+                    return (o, v) => setMethod.Invoke(o, new[] { v });
+                }
+                case FieldInfo field:
+                    return (o, v) => field.SetValue(o, v);
+                default:
+                    throw new ArgumentException();
+            }
+        }
 
         #endregion
     }
