c229e9b47624172fbf345bb7745caa81eba41890
Daniel Crenna <daniel.crenna@gmail.com>
2019-05-21 14:55:50 -0400

    add Attribute access helpers to AccessorMember

--- src/TypeKitchen/AccessorMember.cs
+++ src/TypeKitchen/AccessorMember.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace TypeKitchen
@@ -30,6 +32,48 @@ namespace TypeKitchen
         public AccessorMemberType MemberType { get; }
         public MemberInfo MemberInfo { get; }
 
-        internal bool IsInstanceMethod => CanCall && MemberInfo is MethodInfo method && !method.Name.StartsWith("get_") && !method.Name.StartsWith("set_") && method.DeclaringType != typeof(object);
+        public bool HasAttribute<T>() where T : Attribute
+        {
+            return Attribute.IsDefined(MemberInfo, typeof(T), true);
+        }
+
+        public bool TryGetAttribute<T>(out T attribute) where T : Attribute
+        {
+            if (!Attribute.IsDefined(MemberInfo, typeof(T), true))
+            {
+                attribute = default;
+                return false;
+            }
+            attribute = Attribute.GetCustomAttribute(MemberInfo, typeof(T), true) as T;
+            return attribute != null;
+        }
+
+        public bool TryGetAttributes<T>(out Attribute[] attributes) where T : Attribute
+        {
+            if (!Attribute.IsDefined(MemberInfo, typeof(T), true))
+            {
+                attributes = default;
+                return false;
+            }
+
+            var get = Attribute.GetCustomAttributes(MemberInfo, typeof(T), true);
+            if (get.Length == 0)
+            {
+                attributes = default;
+                return false;
+            }
+
+            attributes = get;
+            return true;
+        }
+
+        public IEnumerable<T> GetAttributes<T>(bool canInherit = true) where T : Attribute
+        {
+            return Attribute.GetCustomAttributes(MemberInfo, typeof(T), canInherit).Cast<T>();
+        }
+
+        internal bool IsInstanceMethod => CanCall && MemberInfo is MethodInfo method &&
+                                          !method.Name.StartsWith("get_") && !method.Name.StartsWith("set_") &&
+                                           method.DeclaringType != typeof(object);
     }
 }
