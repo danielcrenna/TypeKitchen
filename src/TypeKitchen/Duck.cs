3b138f7d45a38bf1d822ccfb61ed732ae1362203
Daniel Crenna <daniel.crenna@gmail.com>
2019-09-01 02:38:57 -0400

    add duck casting

---
+++ src/TypeKitchen/Duck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TypeKitchen
+{
+	public static class Duck
+	{
+		public static T QuackLike<T>(this object source)
+		{
+			// ReSharper disable once SuggestVarOrType_SimpleTypes (must box to support value types)
+			object target = Activator.CreateInstance(Proxy.Create(typeof(T), ProxyType.Mimic));
+
+			var reader = ReadAccessor.Create(source, AccessorMemberTypes.Fields | AccessorMemberTypes.Properties, AccessorMemberScope.Public, out var ra);
+			var writer = WriteAccessor.Create(target, AccessorMemberTypes.Fields | AccessorMemberTypes.Properties, AccessorMemberScope.Public, out var wa);
+
+			foreach (var member in ra)
+			{
+				var hasValue = reader.TryGetValue(source, member.Name, out var value);
+				var hasSetter = wa.TryGetValue(member.Name, out var wm);
+
+				if (hasValue && hasSetter && wm.CanWrite)
+					writer.TrySetValue(target, member.Name, value);
+			}
+
+			return (T) target;
+		}
+	}
+}
---
+++ test/TypeKitchen.Tests/DuckTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace TypeKitchen.Tests
+{
+	public class DuckTests
+	{
+		[Fact]
+		public void Can_duck_cast_as_interface()
+		{
+			var foo = new Foo { Bar = "Baz" }.QuackLike<IFoo>();
+			Assert.Equal("Baz", foo.Bar);
+		}
+
+		public class Foo
+		{
+			public string Bar { get; set; }
+		}
+
+		public interface IFoo
+		{
+			string Bar { get; }
+		}
+	}
+}
