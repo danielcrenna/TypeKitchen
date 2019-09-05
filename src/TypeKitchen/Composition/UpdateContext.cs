7fe355142c2082b6f804da9232283230793aa4d9
Daniel <daniel.crenna@gmail.com>
2019-09-05 11:52:27 -0400

    Add a state bucket

--- src/TypeKitchen/Composition/UpdateContext.cs
+++ src/TypeKitchen/Composition/UpdateContext.cs
@@ -1,19 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace TypeKitchen.Composition
 {
 	public class UpdateContext
 	{
 		public List<Entity> ActiveEntities { get; }
+		public Queue States { get; }
 
 		public UpdateContext()
 		{
 			ActiveEntities = new List<Entity>();
+			States = new Queue();
 		}
 
+		public void AddState(object state)
+		{
+			States.Enqueue(state);
+		}
+		
 		public void Reset()
 		{
 			ActiveEntities.Clear();
+			States.Clear();
 		}
 	}
 }
