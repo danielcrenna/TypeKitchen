a92f1c77e69ab33db9e44c6715cb39f7a5456b69
Daniel Crenna <daniel.crenna@gmail.com>
2020-02-26 17:53:01 -0500

    add type resolver method

---
+++ src/TypeKitchen/Add.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace TypeKitchen
+{
+	public static class Add
+	{
+		public static IServiceCollection AddTypeResolver(this IServiceCollection services, ILogger logger = null)
+		{
+			services.TryAddSingleton<ITypeResolver>(r => new ReflectionTypeResolver(AppDomain.CurrentDomain.GetAssemblies(), logger));
+			return services;
+		}
+	}
+}
